0f797500fca904622b34d00a0d8d84c4f6c04fee
Babatunde <47985277+0xBabatunde@users.noreply.github.com>
2023-12-24 17:56:35 +0000

    auth fix

--- middleware.ts
+++ middleware.ts
@@ -4,7 +4,6 @@ import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
 export async function middleware(req: NextRequest) {
-  const requestUrl = new URL(req.url);
   const res = NextResponse.next();
   const supabase = createMiddlewareClient({ req, res });
 
