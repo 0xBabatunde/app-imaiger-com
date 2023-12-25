c7e7d0e38c5b76bcf95c825c59b16bab04bec428
Babatunde <47985277+0xBabatunde@users.noreply.github.com>
2023-12-25 00:25:23 +0000

    register

--- components/user-register-form.tsx
+++ components/user-register-form.tsx
@@ -2,10 +2,10 @@
 
 import * as React from "react";
 
-import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
-import { useRouter } from "next-nprogress-bar";
+//import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
+//import { useRouter } from "next-nprogress-bar";
 
-import type { Database } from "@/lib/database.types";
+//import type { Database } from "@/lib/database.types";
 
 import { cn } from "@/lib/utils";
 import { Icons } from "@/components/icons";
@@ -18,11 +18,19 @@ interface UserAuthFormProps extends React.HTMLAttributes<HTMLDivElement> {}
 export function UserAuthForm({ className, ...props }: UserAuthFormProps) {
   const [email, setEmail] = React.useState<string>("");
   const [password, setPassword] = React.useState<string>("");
-  const router = useRouter();
-  const supabase = createClientComponentClient<Database>();
+  //const router = useRouter();
+  //const supabase = createClientComponentClient<Database>();
   const [isLoading, setIsLoading] = React.useState<boolean>(false);
 
   async function handleSignUp(event: React.SyntheticEvent) {
+    event.preventDefault();
+    setIsLoading(true);
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 3000);
+  }
+
+  /*async function handleSignUp(event: React.SyntheticEvent) {
     event.preventDefault();
     setIsLoading(true);
     await supabase.auth.signUp({
@@ -51,11 +59,11 @@ export function UserAuthForm({ className, ...props }: UserAuthFormProps) {
       },
     });
     setIsLoading(false);
-  }
+  }*/
 
   return (
     <div className={cn("grid gap-6", className)} {...props}>
-      <form onSubmit={handleSignUp}>
+      <form method="post" onSubmit={handleSignUp}>
         <div className="grid gap-2">
           <div className="grid gap-1">
             <Label className="sr-only" htmlFor="email">
@@ -90,7 +98,11 @@ export function UserAuthForm({ className, ...props }: UserAuthFormProps) {
               required
             />
           </div>
-          <Button type="submit" disabled={isLoading}>
+          <Button
+            type="submit"
+            formAction="/auth/register"
+            disabled={isLoading}
+          >
             {isLoading && (
               <Icons.spinner className="mr-2 h-4 w-4 animate-spin" />
             )}
@@ -108,12 +120,7 @@ export function UserAuthForm({ className, ...props }: UserAuthFormProps) {
           </span>
         </div>
       </div>
-      <Button
-        variant="outline"
-        type="button"
-        onClick={handleGoogleSignUp}
-        disabled={isLoading}
-      >
+      <Button variant="outline" type="button" disabled={isLoading}>
         {isLoading ? (
           <Icons.spinner className="mr-2 h-4 w-4 animate-spin" />
         ) : (
