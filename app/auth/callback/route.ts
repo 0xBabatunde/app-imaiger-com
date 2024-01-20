63d4e8ae17e1291d5f023737862b6286f1f5cc5a
Babatunde <47985277+0xBabatunde@users.noreply.github.com>
2024-01-20 05:48:18 +0000

    final fix :)

--- app/auth/callback/route.ts
+++ app/auth/callback/route.ts
@@ -1,25 +1,45 @@
-import { createRouteHandlerClient } from "@supabase/auth-helpers-nextjs";
+import { createServerClient, type CookieOptions } from "@supabase/ssr";
+import { type EmailOtpType } from "@supabase/supabase-js";
 import { cookies } from "next/headers";
 import { NextRequest, NextResponse } from "next/server";
 
-export const runtime = "edge";
-export const dynamic = "force-dynamic";
-
 export async function GET(request: NextRequest) {
-  const requestUrl = new URL(request.url);
-  const code = requestUrl.searchParams.get("code");
-  const next = requestUrl.searchParams.get("next") ?? "/";
+  const { searchParams } = new URL(request.url);
+  const token_hash = searchParams.get("token_hash");
+  const type = searchParams.get("type") as EmailOtpType | null;
+  const next = searchParams.get("next") ?? "/";
+  const redirectTo = request.nextUrl.clone();
+  redirectTo.pathname = next;
 
-  if (code) {
+  if (token_hash && type) {
     const cookieStore = cookies();
-    const supabase = createRouteHandlerClient({
-      cookies: () => cookieStore,
+    const supabase = createServerClient(
+      process.env.NEXT_PUBLIC_SUPABASE_URL!,
+      process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+      {
+        cookies: {
+          get(name: string) {
+            return cookieStore.get(name)?.value;
+          },
+          set(name: string, value: string, options: CookieOptions) {
+            cookieStore.set({ name, value, ...options });
+          },
+          remove(name: string, options: CookieOptions) {
+            cookieStore.delete({ name, ...options });
+          },
+        },
+      }
+    );
+
+    const { error } = await supabase.auth.verifyOtp({
+      type,
+      token_hash,
     });
-    const { error } = await supabase.auth.exchangeCodeForSession(code);
     if (!error) {
-      return NextResponse.redirect(new URL(`/${next.slice(1)}`, request.url));
+      return NextResponse.redirect(redirectTo);
     }
   }
 
-  return NextResponse.redirect(requestUrl.origin);
+  redirectTo.pathname = "/signin";
+  return NextResponse.redirect(redirectTo);
 }
