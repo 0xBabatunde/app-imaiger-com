041063781f072ba54628fe6fadd8b565cced8048
Babatunde <47985277+0xBabatunde@users.noreply.github.com>
2024-01-20 05:06:10 +0000

    callback fix

--- app/auth/callback/route.ts
+++ app/auth/callback/route.ts
@@ -8,13 +8,17 @@ export const dynamic = "force-dynamic";
 export async function GET(request: NextRequest) {
   const requestUrl = new URL(request.url);
   const code = requestUrl.searchParams.get("code");
+  const next = requestUrl.searchParams.get("next") ?? "/";
 
   if (code) {
     const cookieStore = cookies();
     const supabase = createRouteHandlerClient({
       cookies: () => cookieStore,
     });
-    await supabase.auth.exchangeCodeForSession(code);
+    const { error } = await supabase.auth.exchangeCodeForSession(code);
+    if (!error) {
+      return NextResponse.redirect(new URL(`/${next.slice(1)}`, request.url));
+    }
   }
 
   return NextResponse.redirect(requestUrl.origin);
