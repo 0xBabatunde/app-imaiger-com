0553db240be6d6556ab86ca5b2a856c0e1d0f397
Babatunde <47985277+0xBabatunde@users.noreply.github.com>
2024-01-20 03:49:51 +0000

    minor fix

--- components/mobile-navbar.tsx
+++ components/mobile-navbar.tsx
@@ -34,6 +34,28 @@ export function MobileNav() {
           />
         </Link>
 
+        <div className="mx-auto flex items-center space-x-2">
+          <svg
+            width="15"
+            height="15"
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              fill="#6b7280"
+              d="M12 13a1.49 1.49 0 0 0-1 2.61V17a1 1 0 0 0 2 0v-1.39A1.49 1.49 0 0 0 12 13Zm5-4V7A5 5 0 0 0 7 7v2a3 3 0 0 0-3 3v7a3 3 0 0 0 3 3h10a3 3 0 0 0 3-3v-7a3 3 0 0 0-3-3ZM9 7a3 3 0 0 1 6 0v2H9Zm9 12a1 1 0 0 1-1 1H7a1 1 0 0 1-1-1v-7a1 1 0 0 1 1-1h10a1 1 0 0 1 1 1Z"
+            />
+          </svg>
+          <Link
+            id="upgrade"
+            href="https://imaiger.com/pricing"
+            target="_blank"
+            className="text-sm font-medium transition-colors hover:text-primary"
+          >
+            Upgrade to unlock
+          </Link>
+        </div>
+
         <DropdownMenu>
           <DropdownMenuTrigger>
             <Icons.avatar className="h-8 w-8" />
