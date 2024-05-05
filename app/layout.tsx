04ca8210e1428c9836986cfc041442532b819402
Babatunde <47985277+0xBabatunde@users.noreply.github.com>
2024-05-05 22:47:54 +0100

    add clarity

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import Providers from "@/components/Providers";
+import MicrosoftClarity from "@/components/MicrosoftClarity";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -21,7 +22,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Providers>{children}</Providers>
+        <Providers>
+          {children}
+          <MicrosoftClarity />
+        </Providers>
       </body>
     </html>
   );
